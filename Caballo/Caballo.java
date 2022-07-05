a9f7be5960fc1aa8d15643c5d205b3ff7bd10efd
heylie19 <106571599+heylie19@users.noreply.github.com>
2022-07-04 22:18:58 -0600

    Add files via upload

---
+++ Caballo/Caballo.java
@@ -0,0 +1,102 @@
+
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class Caballo
+{  
+  private int x = 1;  
+  private int y = 1;
+  private int tamano = 8; 
+  private int contador = 1; 
+  int ciclos = 0;                           
+  int intentos_fallidos = 0; 
+
+Scanner entrada = new Scanner(System.in);
+ 
+public void C() 
+{  
+  Random aleatorio = new Random();
+
+  int Arreglo[][] = new int[tamano + 1][tamano +1];
+
+  Arreglo[1][1] = 1;
+  int dado1;
+  int dado2;
+  int casillas_requeridas = 0;
+  
+  System.out.println("\nCuantas casillas desea que recorra?");
+  System.out.printf("\nSi pide mas de %d el programa no terminara nunca:\n", tamano*tamano);
+  casillas_requeridas = entrada.nextInt();
+  
+
+  while ( contador < casillas_requeridas )
+  {
+      intentos_fallidos++;  
+    contador = 1;    
+    int ciclos = 0;   
+
+    for ( int s = 0; s <= tamano; s++ )
+    {       
+        for ( int t = 0; t <= tamano; t++ )
+        Arreglo[s][t] = 0; 
+    }       
+    x = 1;
+    y = 1;
+ 
+    Arreglo[1][1] = 1;
+
+    while ( 1000 != ciclos)
+    {  
+      ciclos++;
+      dado1 = 1 + aleatorio.nextInt(8);
+      dado2 = 1 + aleatorio.nextInt(8);
+
+    if ( Math.abs(Math.abs(x) - Math.abs(dado1)) == 2) 
+    { 
+    if ( Math.abs(Math.abs(y) - Math.abs(dado2)) == 1  )
+ 
+    if ( 0 == Arreglo[dado1][dado2])
+    {  
+      Arreglo[dado1][dado2] = ++contador;   
+      x = dado1;
+      y = dado2;
+      ciclos = 0;
+  }   
+}  
+
+ 
+if ( Math.abs(Math.abs(x) - Math.abs(dado1)) == 1) 
+{  
+  if ( Math.abs(Math.abs(y) - Math.abs(dado2)) == 2  )
+  if ( 0 == Arreglo[dado1][dado2] )
+  {    
+    Arreglo[dado1][dado2] = ++contador; 
+    x = dado1;
+    y = dado2;
+    ciclos = 0;
+  }  
+}  
+ 
+}  
+}    
+
+    System.out.println("\nLISTO!");
+    System.out.printf("\nSe recorrieron %d casillas.\n", contador);
+    Imprime( Arreglo );
+
+}   
+
+ 
+public void Imprime(int B[][])
+{     
+  for ( int k = 1; k <= 8; k++ )
+  {
+    for ( int j = 1; j <= 8; j++)
+  {
+    System.out.printf("%5d", B[k][j]);
+  }  
+    System.out.println("\n");
+  }
+}     
+}    
